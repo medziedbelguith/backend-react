552c36557956793d22b0e87d6d93c5cba7ee9a91
medziedbelguith <medziedbelguith@gmail.com>
2022-07-27 12:16:45 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,112 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useEffect, useState } from 'react';
+import {app,database} from './firebaseConfig';
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword
+} from 'firebase/auth';
+import {collection,addDoc,getDocs,doc,updateDoc,deleteDoc} from 'firebase/firestore'
+
 function App() {
+  const [array,setArray]=useState([]);
+  const [data, setData] = useState({
+    name:'',
+    email: '',
+    password: ''
+  })
+  const auth=getAuth();
+  const dbInstance=collection(database,'users')
+  const handleInputs = (event) => {
+    let inputs = { [event.target.name]: event.target.value }
+
+    setData({ ...data, ...inputs })
+  }
+  const handleSubmit=()=>{
+   addDoc(dbInstance,data)
+   .then(()=>{
+     alert('Data Sent')
+   })
+   .catch((err)=>{
+    alert(err.message)
+   })
+  }
+  const getData=async()=>{
+   const data=await getDocs(dbInstance)
+   setArray(data.docs.map((item)=>{
+     return {...item.data(),id:item.id}
+   }))
+  }
+  const updateData=(id)=>{
+    let dataToUpdate=doc(database,'users',id)
+    updateDoc(dataToUpdate,{
+      name:"Ashih",
+      email:"ashishisbalh@gmail.com",
+      password:"abcdef"
+    }).then(()=>{
+      alert('Data Updated')
+      getData()
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
+  const deleteData=(id)=>{
+    let dataTodelete=doc(database,'users',id)
+    deleteDoc(dataTodelete)
+    .then(()=>{
+      alert('Data Deleted')
+      getData()
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
+  useEffect(()=>{
+   getData()
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <div className="App-header">
+       
+      <input
+        placeholder="Name"
+        name="name"
+        type="text"
+        className="input-fields"
+        onChange={event => handleInputs(event)}
+      />
+    <input
+        placeholder="Email"
+        name="email"
+        type="email"
+        className="input-fields"
+        onChange={event => handleInputs(event)}
+      />
+      <input
+        placeholder="Password"
+        name="password"
+        type="password"
+        className="input-fields"
+        onChange={event => handleInputs(event)}
+      /> 
+      <button onClick={getData}>Get Data</button>
+    
+      {array.map((item)=>{
+        return(
+     <div><p>{item.name}</p>
+     <p>{item.email}</p>
+     <p>{item.password}</p>
+     <button onClick={()=>updateData(item.id)}>Update</button>
+     <button onClick={()=>deleteData(item.id)}>Delete</button>
+     <hr/>
+     </div>
+     )
+   })}
     </div>
   );
 }
